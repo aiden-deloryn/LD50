c96a49faa07fd1321338a8cad97b30955e02af04
Nero <zhuzibajia@gmail.com>
2022-04-02 23:13:00 +1000

    Aiming And Shooting 2
    fixed bullet direction bug
    (todo: bullet shredder)

--- Assets/BulletImpl.cs
+++ Assets/BulletImpl.cs
@@ -8,18 +8,27 @@ public class BulletImpl : MonoBehaviour
     private Camera mainCam;
     private Rigidbody2D rb;
     public float force;// gives the bullet some velocity
-
+    public Transform bulletOut;
+    public Transform playerAt;
     // Start is called before the first frame update
     void Start()
     {
+        bulletOut = GameObject.Find("BulletOut").transform;
+        playerAt = GameObject.Find("Player").transform;
         mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
         rb = GetComponent<Rigidbody2D>();
         mousePositionNow = mainCam.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 direction = mousePositionNow - transform.position;
+        Vector3 direction = bulletOut.position - playerAt.position;
+        //Vector3 direction = mousePositionNow - transform.position;
         Vector3 rotation = transform.position - mousePositionNow;
         rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
         float rot = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0, 0, rot + 90);//why + 90?
+
+
+        transform.rotation = Quaternion.Euler(0, 0 , rot);//why + 90?
+        transform.position = transform.position + bulletOut.position;
+        Debug.Log("bullet rotation: " + transform.rotation);
+        Debug.Log("bullet out point: " + transform.position);
     }
 
     // Update is called once per frame
