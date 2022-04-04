80b3923b8ceb9b7a38a890ede84749c564a606bd
Nero <zhuzibajia@gmail.com>
2022-04-04 16:04:34 +1000

    Enable Respawn by Reloading Scene

--- Assets/GameOverScreen.cs
+++ Assets/GameOverScreen.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameOverScreen : MonoBehaviour
 {
     public void Setup()
     {
         gameObject.SetActive(true);
+    }
+
+    public void RespawnButton()
+    {
+        SceneManager.LoadScene("Aiden");
     }
 }
